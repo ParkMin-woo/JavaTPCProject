4566ed59ac7d93cd7016d253a13f039f612b4325
Park Minwoo <lwmwpark3523@gmail.com>
2020-11-16 01:39:44 +0900

    memberList.json 만들어서 실습해봄.

---
+++ src/com/tpc/second/TPCProject05.java
@@ -0,0 +1,46 @@
+package com.tpc.second;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class TPCProject05 {
+	public static void main(String[] args) {
+		// MemberDTO를 가지고 JSONObject를 만들어 보자.
+		// 멤버는 이름, 나이, 성별, 주소, 번호로 구성
+		JSONObject member = new JSONObject();
+		member.put("name", "박민우");
+		member.put("age", 29);
+		member.put("gender", "M");
+		member.put("address", "경북 포항시");
+		member.put("phone", "01048015126");
+		System.out.println("member1 => " + member);
+		JSONArray memberArray = new JSONArray();
+		memberArray.put(member);
+		System.out.println("memberArray1 => " + memberArray);
+		// memberList.add(new MemberDTO("강희애" , 28 , "F" , "경기 수원시" , "01048768395"));
+		// memberList.add(new MemberDTO("정구" , 49 , "M" , "경기 하남시" , "01028723197"));
+		
+		member = new JSONObject();
+		member.put("name", "강희애");
+		member.put("age", 28);
+		member.put("gender", "F");
+		member.put("address", "경기 수원시");
+		member.put("phone", "01048768395");
+		System.out.println("member2 => " + member);
+		memberArray.put(member);
+		System.out.println("memberArray2 => " + memberArray);
+		
+		member = new JSONObject();
+		member.put("name", "정구");
+		member.put("age", 49);
+		member.put("gender", "M");
+		member.put("address", "경기 하남시");
+		member.put("phone", "01028723197");
+		System.out.println("member3 => " + member);
+		memberArray.put(member);
+		System.out.println("memberArray3 => " + memberArray);
+		JSONObject memberList = new JSONObject();
+		memberList.put("memberList", memberArray);
+		System.out.println("memberList => \n" + memberList.toString(2));
+	}
+}
---
+++ src/com/tpc/second/TPCProject06.java
@@ -0,0 +1,40 @@
+package com.tpc.second;
+
+import java.io.InputStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class TPCProject06 {
+	public static void main(String[] args) {
+		// kr.bit.file에 있는 memberList.json의 파일을 읽어서 파일 내의 데이터를 가져오는 실습을 해보자.
+		String filePath = "/kr/bit/file/memberList.json";
+		InputStream inputStream = TPCProject06.class.getResourceAsStream(filePath);
+		System.out.println("inputStream => " + inputStream);
+		if(inputStream == null) {
+			throw new NullPointerException("Cannot Find Resource File.");
+		}
+		JSONTokener tokener = new JSONTokener(inputStream);
+		System.out.println("tokener => " + tokener);
+		JSONObject memberList = new JSONObject(tokener);
+		System.out.println("memberList => " + memberList);
+		JSONArray memberArray = (JSONArray) memberList.get("memberList");
+		System.out.println("memberArray => " + memberArray);
+		for(int i = 0 ; i < memberArray.length() ; i++) {
+			JSONObject member = (JSONObject) memberArray.get(i);
+			// System.out.println("member => " + member);
+			System.out.print(member.get("name") + "\t");
+			System.out.print(member.get("age") + "\t");
+			System.out.print(member.get("gender") + "\t");
+			System.out.print(member.get("address") + "\t");
+			System.out.println(member.get("phone"));
+		}
+		/*
+		for(JSONObject member : memberArray) {
+			
+		}
+		*/
+		
+	}
+}
