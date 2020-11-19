bd566dda4b32c111014712d3bdca16eb86083e6a
Park Minwoo <lwmwpark3523@gmail.com>
2020-11-19 22:35:16 +0900

    소스 코드 수정

--- src/kr/bit/api/NaverStaticMapAPI.java
+++ src/kr/bit/api/NaverStaticMapAPI.java
@@ -12,29 +12,53 @@ import java.net.URLEncoder;
 import java.util.Date;
 
 public class NaverStaticMapAPI {
+	
+	private String clientId;
+	private String clientSecretKey;
+	private static final String NAVER_STATIC_MAP_URL = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?";
+	
+	/**
+	 * 
+	 */
+	public NaverStaticMapAPI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param clientId
+	 * @param clientSecretKey
+	 */
+	public NaverStaticMapAPI(String clientId, String clientSecretKey) {
+		super();
+		this.clientId = clientId;
+		this.clientSecretKey = clientSecretKey;
+	}
+
 	public void staticMapAPI(String x , String y , String address) {
-		System.out.println("x => " + x);
-		System.out.println("y => " + y);
-		System.out.println("address => " + address);
-		String naverApiUrl = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?";
+		// System.out.println("x => " + x);
+		// System.out.println("y => " + y);
+		// System.out.println("address => " + address);
+		// String naverApiUrl = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?";
 		// 네이버 클라우드 플랫폼을 만들었을 때의 클라이언트id, secret key를 담을 변수를 선언한다.
 		// 절대 이 부분은 git에 commit 후 merge시 공개하지 않는다.
 		// 개인정보 유출의 위험이 있기 때문이다.
+		/*
 		String clientId = "네이버 클라우드 플랫폼에서 application 등록시 발급받은 클라이언트id";		// 네이버 클라우드 플랫폼에서 application 등록시 발급받은 클라이언트id
 		String clientSecretKey = "네이버 클라우드 플랫폼에서 application 등록시 발급받은 secret key";	// 네이버 클라우드 플랫폼에서 application 등록시 발급받은 secret key
+		*/
 		try {
 			String position = URLEncoder.encode(x + " " + y, "UTF-8");
-			String url = naverApiUrl;
+			String url = NAVER_STATIC_MAP_URL;
 			url += "center=" + x + "," + y;
 			url += "&level=16&w=700&h=500";
 			url += "&markers=type:t|size:mid|pos:" + position + "|label:" + URLEncoder.encode(address , "UTF-8");
-			System.out.println("11111");
+			// System.out.println("11111");
 			URL connectedUrl = new URL(url);
-			System.out.println("22222");
+			// System.out.println("22222");
 			HttpURLConnection connection = (HttpURLConnection) connectedUrl.openConnection();
 			connection.setRequestMethod("GET");
-			connection.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
-			connection.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecretKey);
+			connection.setRequestProperty("X-NCP-APIGW-API-KEY-ID", this.clientId);
+			connection.setRequestProperty("X-NCP-APIGW-API-KEY", this.clientSecretKey);
 			
 			int responseCode = connection.getResponseCode();
 			if(responseCode == 200) {
